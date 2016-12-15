5c178934edd82438f8afa59092b25cb8e2c94fde
pedronogalesfuentes <99GU5402@G99514208D02TAB.99G.GISS.SS>
2016-12-15 13:32:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a99gu5402/prueba/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a99gu5402.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
